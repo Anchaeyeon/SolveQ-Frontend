e5c089262b9511c17e755fce17a769c6f8d1f922
Anchaeyeon <s2328@e-mirim.hs.kr>
2024-11-20 14:51:43 +0900

    프레임 크기 수정

--- src/solveQ/start.java
+++ src/solveQ/start.java
@@ -3,12 +3,11 @@ package src.solveQ;
 import javax.swing.*;
 public class start extends JFrame {
     public start() {
-        setTitle("300x300 스윙 프레임 만들기");
-        setSize(300,300); // 프레임 크기 300x300
+        setTitle("스윙 프레임 만들기");
+        setSize(1440,1024); // 프레임 크기
         setVisible(true); // 프레임 출력
     }
     public static void main(String[] args) {
         start frame = new start();
     }
-    //찐 마지막 test
 }
