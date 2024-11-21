644ed33709a1cf142c57f12749cd76ace0c8ac27
M1NGJU <s2303@e-mirim.hs.kr>
2024-11-21 15:07:26 +0900

    로고 추가

--- src/solveQ/start.java
+++ src/solveQ/start.java
@@ -2,6 +2,9 @@ package src.solveQ;
 
 import java.awt.*;
 import javax.swing.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 
 public class start extends JFrame {
     public start() {
@@ -17,6 +20,20 @@ public class start extends JFrame {
 
     // 그라데이션을 그리는 커스텀 패널 클래스
     class GradientPanel extends JPanel {
+        private BufferedImage image;
+
+        // 생성자에서 이미지 로드
+        public GradientPanel() {
+            try {
+                // 이미지 파일 경로를 입력하세요.
+                image = ImageIO.read(new File("img/solveQ.png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
@@ -32,6 +49,15 @@ public class start extends JFrame {
             GradientPaint gradient = new GradientPaint(0, 0, startColor, 0, height, endColor);
             g2d.setPaint(gradient);
             g2d.fillRect(0, 0, width, height); // 그라데이션으로 채우기
+
+            // 이미지 그리기 (중앙 정렬)
+            if (image != null) {
+                int imgWidth = image.getWidth();
+                int imgHeight = image.getHeight();
+                int x = (width - imgWidth) / 2; // 화면 가운데 x 좌표
+                int y = (height - imgHeight) / 2; // 화면 가운데 y 좌표
+                g.drawImage(image, x, y, this);
+            }
         }
     }
 
